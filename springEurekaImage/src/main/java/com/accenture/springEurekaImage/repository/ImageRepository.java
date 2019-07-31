45bc384f1bc727fc58186fc8bd96eb66629ca151
Saimonsee <simonmora30@gmail.com>
2019-07-31 12:01:26 -0300

    eliminar carpeta con archivos sobrantes

--- springEurekaImage/src/main/java/com/accenture/springEurekaImage/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.springEurekaImage.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.accenture.springEurekaImage.entities.Images;
-
-public interface ImageRepository extends JpaRepository<Images, Long> {
-
-}
